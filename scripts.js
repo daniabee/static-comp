53a49406c92162fdfb69f35cb38d95bcca4e1d91
Dani Bagley <bagley.a.danielle@gmail.com>
2022-10-28 09:35:21 -0600

    add dynamic

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
-  document.getElementById("mySidebar").style.width = "200px";
-  document.getElementById("main").style.marginLeft = "200px";
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
 }
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
